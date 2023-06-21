a21f8f5ba4695de7a7abc4d42819d573296b5574
BeAWitch <109846146+BeAWitch@users.noreply.github.com>
2023-06-21 15:43:04 +0800

    引擎上传

---
+++ premake5.lua
@@ -0,0 +1,139 @@
+-- 工作空间名称
+workspace "Hazel" 
+	-- 架构
+	architecture "x86_64"
+	startproject "Sandbox"
+
+	-- 配置
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+-- 自定义变量 输出目录
+outputdir="%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
+
+include "Hazel/vendor/GLFW"
+
+
+project "Hazel"
+	location "Hazel"
+	kind "SharedLib"
+	language "C++"
+
+	-- 目标目录
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	-- 中间目录
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "hzpch.h"
+	pchsource "Hazel/src/hzpch.cpp"
+
+	-- 包含文件 ** 递归搜索标记
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	-- 包含目录
+	includedirs
+	{
+		-- Hazel核心目录, 加入包含目录, 方便使用
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
+
+	links
+	{
+		"GLFW",
+		"opengl32.lib",
+		"Dwmapi.lib"
+	}
+
+	-- 过滤器
+	filter "system:windows"
+
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS",
+			"HZ_BUILD_DLL",
+			"_WINDLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/src/**.hpp"
+	}
+
+	includedirs
+	{
+		"Hazel/src/Hazel/vendor/spdlog/include",
+		"Hazel/vendor/spdlog/include",
+		"Hazel/src" 
+	}
+
+	links
+	{
+		"Hazel"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
