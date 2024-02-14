de742bc7656d28b7a76b5e464b5cd142faf855a5
BeAWitch <109846146+BeAWitch@users.noreply.github.com>
2024-02-14 15:56:23 +0800

    Propertise Panel

--- Hazelnut/src/Panels/SceneHierarchyPanel.h
+++ Hazelnut/src/Panels/SceneHierarchyPanel.h
@@ -14,8 +14,11 @@ namespace Hazel {
 		SceneHierarchyPanel(const Ref<Scene> context);
 
 		void SetContext(const Ref<Scene> context);
+
 		void OnImGuiRender();
+	private:
 		void DrawEntityNode(Entity entity);
+		void DrawComponents(Entity entity);
 	private:
 		Ref<Scene> m_Context;
 		Entity m_SelectionContext;
